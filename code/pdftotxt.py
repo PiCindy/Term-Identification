0efcd1592113ec912347f28219e5bd30243780dc
MadCindy <cindypereira93@gmail.com>
2021-11-24 15:08:10 +0100

    Adding comments

--- code/pdftotxt.py
+++ code/pdftotxt.py
@@ -1,10 +1,15 @@
 import glob
 import pdfplumber
 
+# Iterate over files
 for file in glob.glob(r'articles/pdf/*'):
+    # Open file
     with pdfplumber.open(file) as pdf:
+        # Load all pages of the file
         pages = pdf.pages
+        # Create the txt version of the file
         path = file.replace('pdf', 'txt')
         with open(path, 'a') as txt:
+            # Copy content of each page into the txt file
             for page in pages:
                 txt.write(page.extract_text())
