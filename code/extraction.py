2a758ccf7c1ac0ba8a17eaed47bb91c9a812bf19
MadCindy <cindypereira93@gmail.com>
2021-11-24 15:01:25 +0100

    Extraction of terms

--- code/extraction.py
+++ code/extraction.py
@@ -1,1 +1,15 @@
 import spacy
+
+nlp = spacy.load("en_core_web_sm")
+#txt = 'Hello my name is Cindy and you? My name is Justine.'
+#doc = nlp(txt)
+
+vocabulary = set()
+for file in glob.glob('articles/txt/*'):
+    with open(file, 'r') as f:
+        doc = nlp(f.read())
+    for spacy_token in doc:
+        token = spacy_token.text.lower()
+        if not spacy_token.is_stop and token not in vocabulary:
+            vocabulary.add(token)
+print(vocabulary)
