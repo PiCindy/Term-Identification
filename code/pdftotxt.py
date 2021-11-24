7edbb5701349f63dc9144b9a89a0b95dd306374c
MadCindy <cindypereira93@gmail.com>
2021-11-24 14:45:44 +0100

    Rangement de fichiers PDF

---
+++ code/pdftotxt.py
@@ -0,0 +1,11 @@
+import PyPDF2
+import glob
+import pdfplumber
+
+for file in glob.glob(r'articles/pdf/*'):
+    with pdfplumber.open(file) as pdf:
+        pages = pdf.pages
+        path = file.replace('pdf', 'txt')
+        with open(path, 'a') as txt:
+            for page in pages:
+                txt.write(page.extract_text())
