6d00bee43c5fb07dfd1430904932e4d1cad4e4b9
MadCindy <cindypereira93@gmail.com>
2021-11-25 18:54:57 +0100

    POS tagger

--- code/extraction.py
+++ code/extraction.py
@@ -1,27 +1,26 @@
 import spacy
 import glob
-import json
 
-nlp = spacy.load("en_core_web_sm")
+def pos_tagger(text: str):
+    ''' Preprocess a text using spaCy part-of-speech tagger
+    Create file of form "token part-of-speech" one per line
+    input: the text we want to preprocess (string)
+    output: a list with tuples (token lemma part-of-speech)'''
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(text)
+    tokens_pos = []
+    with open("output/tokens_pos.txt", 'a') as f:
+        for token in doc:
+            line = f'{token.text}\t{token.lemma_}\t{token.pos_}\n'
+            f.write(line)
+            tokens_pos.append((token.text, token.lemma_, token.pos))
+    return tokens_pos
 
-# create set
-vocabulary = {}
-# iterate over files
+open("output/tokens_pos.txt", 'w').close()
 for file in glob.glob('articles/txt/*'):
-    # open each file (read mode)
+    print(file)
+    tokens_pos = []
     with open(file, 'r') as f:
-        # using spacy model, read text with it
-        doc = nlp(f.read())
-    # tokenize text
-    for spacy_token in doc:
-        # change token into string and lowercase
-        token = spacy_token.text.lower()
-        # adding tokens in vocabulary (if it is not a stopword and not already in voc)
-        if not spacy_token.is_stop and not spacy_token.is_punct:
-            vocabulary[token] = vocabulary.get(token, 0) + 1
-
-print(len(vocabulary.keys()))
-sorted_voc = {k: v for k, v in sorted(vocabulary.items(), key=lambda item: item[1])[::-1]}
-
-with open('output/vocabulary.json', 'w') as f:
-    json.dump(sorted_voc, f)
+        text = f.read()
+        text = text.replace('\n', ' ')
+        tokens_pos.append(pos_tagger(text))
