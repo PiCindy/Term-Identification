eee16911978ba4b9b65bdba29a33e3f34ec7298c
MadCindy <cindypereira93@gmail.com>
2021-11-25 19:30:39 +0100

    Vocabulary

--- code/frequency.py
+++ code/frequency.py
@@ -3,14 +3,19 @@ import json
 frequencies = {}
 
 with open('output/candidates.txt', 'r') as f:
-    i=0
     for line in f.readlines():
         candidate = line
         frequencies[candidate] = frequencies.get(candidate, 0) + 1
-        i+=1
-        if i==500:
-            break
 
 sorted_freq = {k: v for k, v in sorted(frequencies.items(), key=lambda item: item[1])[::-1]}
+
+five_hundred_voc = {}
+i=0
+for key, value in sorted_freq.items():
+    five_hundred_voc[key] = value
+    i+=1
+    if i==500:
+        break
+
 with open('output/vocabulary.json', 'w') as f:
-    json.dump(sorted_freq, f)
+    json.dump(five_hundred_voc, f)
